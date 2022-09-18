b655272733c03761371fa69d74d40553a45ad3a2
manthi4 <sumanth.phi2000@gmail.com>
2022-09-18 06:58:11 -0400

    helloworld

--- interface.py
+++ interface.py
@@ -8,30 +8,61 @@ from curses import wrapper
 import climage
 from gif_for_cli.execute import execute
 import sys
+from art import *
 
 
 def main(mainwin):
     # f = open("some.txt", "r+")
     # st = f.read()
     # f.close()
-    
-    while(True):
+    curcols, curlines = stdscr.getmaxyx()
+    hewo = text2art("hello!")
+    hewo_w = 30
+    hewo_h = 7
+    def draw_mainwin(mainwin):
         mainwin.clear()
         mainwin.border('|','|', '-','-', '+', 'O', '+', '4')
-        mainwin.addstr(2, 2, "Cols: " +  str(curses.COLS))
-        mainwin.addstr(3, 2, "Lines: " +  str(curses.LINES))
-        # mainwin.addstr(4, 2, st)
-        # execute(os.environ,["spiderlily.png"],sys.stdout)
-        mainwin.refresh()
+        printyloc = 2
+        mainwin.addstr(printyloc, 2, "Cols: " +  str(curcols)) # loc = (y, x) y = cols down , x = lines right
+        mainwin.addstr(printyloc+1, 2, "Lines: " +  str(curlines))
 
-        begin_x = 20; begin_y = 7
-        height = 20; width = 100
+    def create_winny():
+        begin_x = int(curlines/2 - hewo_w/2)
+        begin_y = int(curcols/2 - hewo_h/2)
+        height = hewo_h
+        width = hewo_w #try not use fixed widths
         termage = climage.convert('spiderlily.png', is_unicode=False)
         winny = curses.newwin(height, width, begin_y, begin_x)
+        return winny
+        
+    def draw_winny(winny):
+        winny.clear()
         winny.border('x','x', 'x','x', 'O', 'O', 'O', 'O')
-        winny.addstr(termage)
+        winny.addstr(hewo)
+        # winny.addstr(termage)
+        # winny.refresh()
+
+    
+    winny = create_winny()
+    while(True):
+        # mainwin.clear()
+        # if curlines != str(curses.LINES) or currcols != str(curses.COLS):
+        #     printyloc += 2
+        #     currcols = str(curses.COLS)
+        #     curlines = str(curses.LINES)
+        #     mainwin.addstr(printyloc, 2, "Cols: " +  currcols) # loc = (y, x) y = cols down , x = lines right
+        #     mainwin.addstr(printyloc+1, 2, "Lines: " +  curlines)
+        curcols, curlines = stdscr.getmaxyx()
+        time.sleep(1)
+        draw_mainwin(mainwin)
+        draw_winny(winny)
+        mainwin.refresh()
         winny.refresh()
 
 
-curses.initscr()
+
+
+
+
+stdscr = curses.initscr()
 wrapper(main)
